16a43fcf96e1903e1ee57566c9365c0e70556b92
Lev Chelyadinov <leva181777@gmail.com>
2021-03-19 18:41:51 +0300

    Set up a TypeScript skeleton

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.ts',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'build/stories.js'
+	},
+	plugins: [
+		resolve({
+			browser: true,
+		}),
+		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
