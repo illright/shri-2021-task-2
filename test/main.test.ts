0713c97f40b02f14e484b90e107860b238ae77af
Lev Chelyadinov <leva181777@gmail.com>
2021-03-21 23:16:33 +0300

    Polish up the tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -3,7 +3,8 @@ import outputData from './data/output.json';
 import { prepareData } from '../src/main';
 import type { Entity } from '../src/entities';
 
-test('works', () => {
-  const myOutput = prepareData(inputData as Entity[], { sprintId: 977 });
+test('the example input produces the example output', () => {
+  const exampleSprint = 977;
+  const myOutput = prepareData(inputData as Entity[], { sprintId: exampleSprint });
   expect(myOutput).toEqual(outputData);
 })
