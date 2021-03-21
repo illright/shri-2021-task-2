cfde15baa03b1fb7d7b508eeb1c0d0a2a354defa
Lev Chelyadinov <leva181777@gmail.com>
2021-03-21 14:46:20 +0300

    Fix the TypeScript config of Jest to allow JSON imports

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,3 +1,9 @@
-test('jest works', () => {
-  expect(1).toBe(1 as number);
+import inputData from './data/input.json';
+import outputData from './data/output.json';
+import { prepareData } from '../src/main';
+import type { Entity } from '../src/entities';
+
+test('works', () => {
+  const myOutput = prepareData(inputData as Entity[], { sprintId: 977 });
+  expect(myOutput).toBe(outputData);
 })
