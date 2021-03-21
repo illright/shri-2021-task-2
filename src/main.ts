ebdea2144c229b9851a14e078e6bed37092e92a4
Lev Chelyadinov <leva181777@gmail.com>
2021-03-21 12:48:56 +0300

    Construct the diagram slide data and refactor into functions

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import binarySearch from 'binary-search';
 import Heap from 'heap-js';
 import { pluralize, entityPluralizations } from './pluralization';
-import type { Slide, Period, VoteSlide, LeadersSlide, ChartSlide } from './task1';
+import type { Slide, Period, VoteSlide, LeadersSlide, ChartSlide, DiagramSlide } from './task1';
 import type {
   Comment,
   Commit,
@@ -11,6 +11,7 @@ import type {
   Sprint,
   SprintId,
   Summary,
+  SummaryId,
   User,
   UserId,
 } from './entities';
@@ -32,7 +33,6 @@ function relativeToSprint(sprint: Sprint, timestamp: number) {
   return RelativePosition.Within;
 }
 
-
 function withinSprint(timestamp: number, sprint: Sprint) {
   return relativeToSprint(sprint, timestamp) === RelativePosition.Within;
 }
@@ -41,6 +41,160 @@ function getUserID(userOrID: User | UserId) {
   return typeof userOrID === 'number' ? userOrID : userOrID.id;
 }
 
+interface CommitSizes {
+  upTo100: number;
+  upTo500: number;
+  upTo1000: number;
+  moreThan1000: number;
+}
+
+function computeCommitSize(commit: Commit, summaries: Map<SummaryId, Summary>) {
+  let result = 0;
+  for (const summaryOrId of commit.summaries) {
+    const summary = typeof summaryOrId === 'number' ? summaries.get(summaryOrId) : summaryOrId;
+    result += summary.added + summary.removed;
+  }
+  return result;
+}
+
+function incrementSizeCounter(commitSize: number, counters: CommitSizes) {
+  if (commitSize <= 100) {
+    counters.upTo100++;
+  } else if (commitSize <= 500) {
+    counters.upTo500++;
+  } else if (commitSize <= 1000) {
+    counters.upTo1000++;
+  } else {
+    counters.moreThan1000++;
+  }
+}
+
+function buildVoteSlide(
+  sprintID: number,
+  likesThisSprint: Map<UserId, number>,
+  users: Map<UserId, User>,
+): VoteSlide {
+  return {
+    alias: 'vote',
+    data: {
+      title: 'Самый 🔎 внимательный разработчик',
+      subtitle: `Спринт № ${sprintID}`,
+      emoji: '🔎',
+      users: [...likesThisSprint.entries()].map(([id, likeCount]) => {
+        const user = users.get(id);
+        return {
+          id,
+          name: user.name,
+          avatar: user.avatar,
+          valueText: pluralize(likeCount, entityPluralizations.votes),
+        }
+      }),
+    },
+  };
+}
+
+function buildLeadersSlide(
+  sprintID: number,
+  commitsPerUserThisSprint: Map<UserId, number>,
+  users: Map<UserId, User>,
+): LeadersSlide {
+  return {
+    alias: 'leaders',
+    data: {
+      title: 'Больше всего коммитов',
+      subtitle: `Спринт № ${sprintID}`,
+      emoji: '👑',
+      users: [...commitsPerUserThisSprint.entries()].map(([id, commitCount]) => {
+        const user = users.get(id);
+        return {
+          id,
+          name: user.name,
+          avatar: user.avatar,
+          valueText: commitCount.toString(),
+        }
+      }),
+    }
+  };
+}
+
+function buildChartSlide(
+  sprintID: number,
+  commitsPerSprint: Map<SprintId, number>,
+  commitsPerUserThisSprint: Map<UserId, number>,
+  users: Map<UserId, User>,
+): ChartSlide {
+  const commitsHeap = new Heap((a: [number, number], b: [number, number]) => a[1] - b[1]);
+  commitsHeap.push(...commitsPerUserThisSprint.entries());
+  return {
+    alias: 'chart',
+    data: {
+      title: 'Коммиты',
+      subtitle: `Спринт № ${sprintID}`,
+      values: [...commitsPerSprint.entries()].map(([id, commitCount]) => {
+        const period: Period = { title: id.toString(), value: commitCount };
+        if (id === sprintID) {
+          period.active = true;
+        }
+        return period;
+      }),
+      users: commitsHeap.top(3).map(([id, commitCount]) => {
+        const user = users.get(id);
+        return {
+          id,
+          name: user.name,
+          avatar: user.avatar,
+          valueText: commitCount.toString(),
+        }
+      }),
+    }
+  };
+}
+
+function buildDiagramSlide(
+  sprintID: number,
+  commitSizesThisSprint: CommitSizes,
+  commitSizesLastSprint: CommitSizes,
+): DiagramSlide {
+  const totalCommitsThisSprint = (
+    commitSizesThisSprint.upTo100
+    + commitSizesThisSprint.upTo500
+    + commitSizesThisSprint.upTo1000
+    + commitSizesThisSprint.moreThan1000
+  );
+  const totalCommitsLastSprint = (
+    commitSizesLastSprint.upTo100
+    + commitSizesLastSprint.upTo500
+    + commitSizesLastSprint.upTo1000
+    + commitSizesLastSprint.moreThan1000
+  );
+  const totalDifference = totalCommitsThisSprint - totalCommitsLastSprint;
+  const forceSign = true;
+  const categories = [
+    ['> 1001 строки', 'moreThan1000'],
+    ['501 — 1000 строк', 'upTo1000'],
+    ['501 — 1000 строк', 'upTo1000'],
+    ['101 — 500 строк', 'upTo500'],
+    ['1 — 100 строк', 'upTo100'],
+  ];
+  return {
+    alias: 'diagram',
+    data: {
+      title: 'Размер коммитов',
+      subtitle: `Спринт № ${sprintID}`,
+      totalText: pluralize(totalCommitsThisSprint, entityPluralizations.commits),
+      differenceText: `${totalDifference > 0 ? '+' + totalDifference : totalDifference} с прошлого спринта`,
+      categories: categories.map(([title, field]) => ({
+        title,
+        valueText: pluralize(commitSizesThisSprint[field], entityPluralizations.commits),
+        differenceText: pluralize(
+          commitSizesThisSprint[field] - commitSizesLastSprint[field],
+          entityPluralizations.commits,
+          forceSign,
+        ),
+      })),
+    },
+  };
+}
 
 export function prepareData(entities: Entity[], { sprintId }: { sprintId: number }): Slide[] {
   const users = new Map<UserId, User>();
@@ -48,10 +202,11 @@ export function prepareData(entities: Entity[], { sprintId }: { sprintId: number
   const issues: Issue[] = [];
   const comments: Comment[] = [];
   const commits: Commit[] = [];
-  const summaries: Summary[] = [];
+  const summaries = new Map<SummaryId, Summary>();
   const sprints: Sprint[] = [];
 
   let currentSprint: Sprint;
+  let lastSprint: Sprint;
 
   for (const entity of entities) {
     if (entity.type === 'User') {
@@ -65,10 +220,13 @@ export function prepareData(entities: Entity[], { sprintId }: { sprintId: number
     } else if (entity.type === 'Commit') {
       commits.push(entity);
     } else if (entity.type === 'Summary') {
-      summaries.push(entity);
+      summaries.set(entity.id, entity);
     } else {
       if (entity.id === sprintId) {
         currentSprint = entity;
+      }
+      if (entity.id === sprintId - 1) {
+        lastSprint = entity;
       }
       sprints.push(entity);
     }
@@ -85,12 +243,32 @@ export function prepareData(entities: Entity[], { sprintId }: { sprintId: number
     }
   }
 
-  const commitsThisSprint = new Map<UserId, number>();
+  const commitsPerUserThisSprint = new Map<UserId, number>();
   const commitsPerSprint = new Map<SprintId, number>();
+  const commitSizesThisSprint: CommitSizes = {
+    upTo100: 0,
+    upTo500: 0,
+    upTo1000: 0,
+    moreThan1000: 0,
+  };
+  const commitSizesLastSprint: CommitSizes = {
+    upTo100: 0,
+    upTo500: 0,
+    upTo1000: 0,
+    moreThan1000: 0,
+  };
   for (const commit of commits) {
     if (withinSprint(commit.timestamp, currentSprint)) {
       const authorId = getUserID(commit.author);
-      commitsThisSprint.set(authorId, (commitsThisSprint.get(authorId) ?? 0) + 1);
+      commitsPerUserThisSprint.set(authorId, (commitsPerUserThisSprint.get(authorId) ?? 0) + 1);
+
+      const commitSize = computeCommitSize(commit, summaries);
+      incrementSizeCounter(commitSize, commitSizesThisSprint);
+    }
+
+    if (withinSprint(commit.timestamp, lastSprint)) {
+      const commitSize = computeCommitSize(commit, summaries);
+      incrementSizeCounter(commitSize, commitSizesLastSprint);
     }
 
     const sprintIndex = binarySearch(sprints, commit.timestamp, relativeToSprint);
@@ -102,67 +280,27 @@ export function prepareData(entities: Entity[], { sprintId }: { sprintId: number
     }
   }
 
-  const voteSlide: VoteSlide = {
-    alias: 'vote',
-    data: {
-      title: 'Самый 🔎 внимательный разработчик',
-      subtitle: `Спринт № ${sprintId}`,
-      emoji: '🔎',
-      users: [...likesThisSprint.entries()].map(([id, likeCount]) => {
-        const user = users.get(id);
-        return {
-          id,
-          name: user.name,
-          avatar: user.avatar,
-          valueText: pluralize(likeCount, entityPluralizations.votes),
-        }
-      }),
-    },
-  };
-
-  const leadersSlide: LeadersSlide = {
-    alias: 'leaders',
-    data: {
-      title: 'Больше всего коммитов',
-      subtitle: 'Спринт № 213',
-      emoji: '👑',
-      users: [...commitsThisSprint.entries()].map(([id, commitCount]) => {
-        const user = users.get(id);
-        return {
-          id,
-          name: user.name,
-          avatar: user.avatar,
-          valueText: commitCount.toString(),
-        }
-      }),
-    }
-  };
-
-  const commitsHeap = new Heap((a: [number, number], b: [number, number]) => a[1] - b[1]);
-  commitsHeap.push(...commitsThisSprint.entries());
-  const chartSlide: ChartSlide = {
-    alias: 'chart',
-    data: {
-      title: 'Коммиты',
-      subtitle: 'Спринт № 213',
-      values: [...commitsPerSprint.entries()].map(([id, commitCount]) => {
-        const period: Period = { title: id.toString(), value: commitCount };
-        if (id === sprintId) {
-          period.active = true;
-        }
-        return period;
-      }),
-      users: commitsHeap.top(3).map(([id, commitCount]) => {
-        const user = users.get(id);
-        return {
-          id,
-          name: user.name,
-          avatar: user.avatar,
-          valueText: commitCount.toString(),
-        }
-      }),
-    }
-  };
-
-  return [voteSlide, leadersSlide, chartSlide];
+  return [
+    buildVoteSlide(
+      sprintId,
+      likesThisSprint,
+      users,
+    ),
+    buildLeadersSlide(
+      sprintId,
+      commitsPerUserThisSprint,
+      users,
+    ),
+    buildChartSlide(
+      sprintId,
+      commitsPerSprint,
+      commitsPerUserThisSprint,
+      users,
+    ),
+    buildDiagramSlide(
+      sprintId,
+      commitSizesThisSprint,
+      commitSizesLastSprint,
+    ),
+  ];
 }
--- src/pluralization.ts
+++ src/pluralization.ts
@@ -9,17 +9,24 @@ export const entityPluralizations: Record<string, PluralizationRule> = {
     one: 'голос',
     two: 'голоса',
     five: 'голосов',
+  },
+  commits: {
+    one: 'коммит',
+    two: 'коммита',
+    five: 'коммитов',
   }
 }
 
-export function pluralize(amount: number, rule: PluralizationRule) {
+export function pluralize(amount: number, rule: PluralizationRule, forceSign = false) {
+  const amountStr = (forceSign && amount > 0) ? `+${amount}` : amount.toString();
+
   if (amount % 10 === 1 && (amount % 100 - 1) != 10) {
-    return `${amount} ${rule.one}`;
+    return `${amountStr} ${rule.one}`;
   }
 
   if (amount % 10 >= 2 && amount % 10 <= 4 && (amount % 100 - amount % 10) != 10) {
-    return `${amount} ${rule.two}`;
+    return `${amountStr} ${rule.two}`;
   }
 
-  return `${amount} ${rule.five}`;
+  return `${amountStr} ${rule.five}`;
 }
